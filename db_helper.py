ebed8ca28cd3c5131298d583ec00c3c936602394
wu <w19961009@126.com>
2018-03-27 18:55:11 +0800

    fix

--- db_helper.py
+++ db_helper.py
@@ -1,14 +1,22 @@
 # coding: utf-8
 from user import User
+import mysql.connector
+
 
 # 注释：数据库操作
 
 def init():
-    """
-初始化
-    :return:
-    """
+    connection = mysql.connector.connect(user='root', password='root', database='file_server_db', use_unicode=True)
+    cursor = connection.cursor()
     return
+
+
+def check_create():
+    pass
+
+
+def dispose():
+    pass
 
 
 def find_user_by_username(username):
