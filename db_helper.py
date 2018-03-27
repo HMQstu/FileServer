7fd51984709d57335e43ad840a272fe50bbed44c
humengqiu <18117854900@163.com>
2018-03-27 20:13:22 +0800

    数据库操作

--- db_helper.py
+++ db_helper.py
@@ -5,13 +5,25 @@ import mysql.connector
 
 # 注释：数据库操作
 
+connection = None
+
 def init():
+    global connection
     connection = mysql.connector.connect(user='root', password='root', database='file_server_db', use_unicode=True)
-    cursor = connection.cursor()
     return
 
 
 def check_create():
+    sql = '''
+        create table if not exists user(
+            id bigint primary key auto_increment,
+            username varchar(255),
+            password varchar(255),
+            role int,
+            mail varchar(40),
+            phone varchar(15)
+        );
+    '''
     pass
 
 
@@ -20,16 +32,23 @@ def dispose():
 
 
 def find_user_by_username(username):
-    # 查询数据库，填充user 执行SQL语句
+    # SQL语句
+    sql = 'select username,password,role,mail,phone from user where username=%s' % username
+    # 执行SQL
+    # 填充User
     u = User()
+    # 返回User
     return None
 
 
 def insert_user(user):
     # 执行插入语句
+    sql = "insert into user(id,username,password,role,mail,phone) values(0,'%s','%s',%d,'%s','%s')" \
+            % user.username, user.password,user.role,user.mail,user.phone
     pass
 
 
 def remove_user_by_username(username):
     # 执行删除语句
+    sql = 'delete from user where username = %s' % username
     pass
